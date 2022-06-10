609be7f6fdd7a0b1adc16758251de404ab542d96
Ganesh V <me@ganesh.one>
2022-06-10 19:17:40 +0530

    Z invert, max pos changed

--- Marlin-bugfix-2.0.x/Marlin/Configuration.h
+++ Marlin-bugfix-2.0.x/Marlin/Configuration.h
@@ -1351,9 +1351,9 @@
 // @section machine
 
 // Invert the stepper direction. Change (or reverse the motor connector) if an axis goes the wrong way.
-#define INVERT_X_DIR false//true
-#define INVERT_Y_DIR false//true
-#define INVERT_Z_DIR false//true
+#define INVERT_X_DIR    false//false
+#define INVERT_Y_DIR    false//false
+#define INVERT_Z_DIR    true//true
 //#define INVERT_I_DIR false
 //#define INVERT_J_DIR false
 //#define INVERT_K_DIR false
@@ -1361,8 +1361,8 @@
 // @section extruder
 
 // For direct drive extruder v9 set to true, for geared extruder set to false.
-#define INVERT_E0_DIR true//false
-#define INVERT_E1_DIR false//true
+#define INVERT_E0_DIR   false//false
+#define INVERT_E1_DIR   true//true
 #define INVERT_E2_DIR false
 #define INVERT_E3_DIR false
 #define INVERT_E4_DIR false
@@ -1399,8 +1399,8 @@
 // @section machine
 
 // The size of the printable area
-#define X_BED_SIZE 395//375
-#define Y_BED_SIZE 395
+  #define X_BED_SIZE  395
+  #define Y_BED_SIZE  400
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
@@ -1408,7 +1408,7 @@
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
-#define Z_MAX_POS 400
+#define Z_MAX_POS 415
 //#define I_MIN_POS 0
 //#define I_MAX_POS 50
 //#define J_MIN_POS 0
