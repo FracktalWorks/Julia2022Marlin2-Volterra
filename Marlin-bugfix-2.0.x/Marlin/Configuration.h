4f6fdfa756ab26fb267cabed2f25ee7724b46842
Ganesh V <me@ganesh.one>
2023-05-05 10:21:30 +0530

    made max temp 460 for E0 and E1
    made max temp 460 for E0 and E1

--- Marlin-bugfix-2.0.x/Marlin/Configuration.h
+++ Marlin-bugfix-2.0.x/Marlin/Configuration.h
@@ -565,8 +565,8 @@
 // Above this temperature the heater will be switched off.
 // This can protect components from overheating, but NOT from shorts and failures.
 // (Use MINTEMP for thermistor short/failure protection.)
-#define HEATER_0_MAXTEMP 410//275
-#define HEATER_1_MAXTEMP 410//275
+#define HEATER_0_MAXTEMP 460//275
+#define HEATER_1_MAXTEMP 460//275
 #define HEATER_2_MAXTEMP 120//275
 #define HEATER_3_MAXTEMP 80//275
 #define HEATER_4_MAXTEMP 275
